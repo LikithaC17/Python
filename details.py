a298a8cfad460afea46ce97ea01622c1da44f187
Likitha.C <likithachannegowdagmail.com>
2026-07-02 18:30:52 +0530

    Commit

---
+++ details.py
@@ -0,0 +1,9 @@
+name = input("Enter the name: ")
+age = input("Enter the age: ")
+branch = input("Enter the Branch name: ")
+usn = input("Enter the USN: ")
+
+print("Name:", name)
+print("Age:", age)
+print("Branch:", branch)
+print("USN:", usn)
