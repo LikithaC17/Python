b830f38bdb98e26020cceba3fe8f1482a4f0245c
Likitha.C <likithachannegowdagmail.com>
2026-07-03 22:21:53 +0530

    Commit

---
+++ operation.py
@@ -0,0 +1,10 @@
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+sum=a+b
+div=a/b
+mul=a*b
+sub=a-b
+print("sum of a and b is:",sum)
+print("div of a and b is:",div)
+print("mul of a and b is:",mul)
+print("sub of a and b is:",sub)
