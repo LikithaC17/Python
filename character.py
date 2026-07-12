fd8ddbad2994a9858f5ddbb0ef29f690b37ea118
Likitha.C <likithachannegowdagmail.com>
2026-07-12 11:35:31 +0530

    Commit

---
+++ character.py
@@ -0,0 +1,7 @@
+ch=input("Enter a character:")
+if ch.isupper():
+    print("The character is Uppercase")
+elif ch.islower():
+    print("The character is Lowercase")
+else:
+    print("It is not an alphabet")
