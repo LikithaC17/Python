23f4c260cc1bde09c1c82e9ef2d010582def51aa
Likitha.C <likithachannegowdagmail.com>
2026-07-22 22:02:11 +0530

    Commit

---
+++ neon.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number:"))
+square=n*n
+sum=0
+while square>0:
+    digit=square%10
+    sum=sum+digit
+    square=square//10
+if sum==n:
+    print("Neon Number")
+else:
+    print("Not a Neon Number")
