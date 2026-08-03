3ac25e0dbb78e2d74ba32bfc75f6756c3a1b3b30
Likitha.C <likithachannegowdagmail.com>
2026-08-03 13:34:12 +0530

    Commit

---
+++ compressstring.py
@@ -0,0 +1,10 @@
+text=input("Enter a string:")
+result=""
+count=1
+for i in range(len(text)):
+    if i<len(text)-1 and text[i]==text[i+1]:
+        count=count+1
+    else:
+        result=result+text[i]+str(count)
+        count=1
+print(result)
