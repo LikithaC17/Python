38a036c00a277b58bc0de395cfff5b2b4f12ddd6
Likitha.C <likithachannegowdagmail.com>
2026-08-07 19:30:55 +0530

    Commit

---
+++ commonelements.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter number of elements in first list:"))
+list1=[]
+for i in range(n1):
+    list1.append(int(input()))
+n2=int(input("Enter number of elements in second list:"))
+list2=[]
+for i in range(n2):
+    list2.append(int(input()))
+common=list(set(list1)&set(list2))
+print(common)
