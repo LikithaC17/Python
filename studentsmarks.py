01e1f7e643d3aece4f3df2b1cd013ba86fdff347
Likitha.C <likithachannegowdagmail.com>
2026-08-15 23:19:33 +0530

    Commit

---
+++ studentsmarks.py
@@ -0,0 +1,6 @@
+student={"name":"Likitha","Python":85,"Maths":90,"Science":88}
+total=student["Python"]+student["Maths"]+student["Science"]
+average=total/3
+print("Name:",student["name"])
+print("Total:",total)
+print("Average:",average)
