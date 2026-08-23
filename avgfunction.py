3226ce6a222dfe5673057e38388b9ca254adf17b
Likitha.C <likithachannegowdagmail.com>
2026-08-23 11:46:11 +0530

    Commit

---
+++ avgfunction.py
@@ -0,0 +1,7 @@
+def average(a,b,c):
+    return (a+b+c)/3
+
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+c=float(input("Enter third number:"))
+print("Average:",average(a,b,c))
